ba803ed655f5892781910283c20f2161785c22ea
sandeep <Surya.kant@sapple.co.in>
2024-03-14 11:01:54 +0530

    change

--- src/app/client/home/home.component.ts
+++ src/app/client/home/home.component.ts
@@ -9,8 +9,20 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  activeImageIndex = 0;
+  images: string[] = [
+    'assets/image/car7.png',
+    'assets/image/car8.png',
+    'assets/image/car9.png',
+    'assets/image/car10.png',
+    'assets/image/car11.png',
+    'assets/image/car12.png',
+    'assets/image/car13.png',
+    'assets/image/car14.png',
+  ];
 
   ngOnInit(): void {
+    this.showImage(this.activeImageIndex);
   }
 
 
@@ -34,33 +46,46 @@ export class HomeComponent implements OnInit {
   }
 
   slides = [
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   ];
   slideConfig = {
-  "slidesToShow": 4, 
-  "slidesToScroll": 1,
-  autoplay: true, // Add this line for autoplay
-  autoplaySpeed: 2000, // Adjust autoplay speed as needed
-  arrows: true,
-  dots: true,
-  infinite: true,
-  fade: false,
-  touchMove: true
+    "slidesToShow": 3,
+    "slidesToScroll": 1,
+    autoplay: true, // Add this line for autoplay
+    autoplaySpeed: 2000, // Adjust autoplay speed as needed
+    arrows: true,
+    dots: true,
+    infinite: true,
+    fade: false,
+    touchMove: true
   };
+
+  showImage(index: number): void {
+    if (index >= 0 && index < this.images.length) {
+      this.activeImageIndex = index;
+      console.log(this.activeImageIndex )
+    }
+  }
+
   
+  nextImage(): void {
+    this.activeImageIndex = (this.activeImageIndex + 1) % this.images.length;
+  }
 
-
+  prevImage(): void {
+    this.activeImageIndex = (this.activeImageIndex - 1 + this.images.length) % this.images.length;
+  }
 
 }
