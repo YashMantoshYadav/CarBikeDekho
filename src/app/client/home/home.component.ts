f01d7937b5707a2235d3070efcbbb28b0ad7877d
sandeep <Surya.kant@sapple.co.in>
2024-03-14 15:02:51 +0530

    o home

--- src/app/client/home/home.component.ts
+++ src/app/client/home/home.component.ts
@@ -61,9 +61,9 @@ export class HomeComponent implements OnInit {
     { img: "https://images.unsplash.com/photo-1692181415175-c1f7ecc826ae?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   ];
   slideConfig = {
-    "slidesToShow": 3,
-    "slidesToScroll": 1,
-    autoplay: true, // Add this line for autoplay
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false, // Add this line for autoplay
     autoplaySpeed: 2000, // Adjust autoplay speed as needed
     arrows: true,
     dots: true,
